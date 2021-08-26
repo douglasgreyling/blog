94f202e1a068dd14ecb42b7f4b2dc826860d209a
Douglas Greyling <greyling.douglas@gmail.com>
2021-08-26 19:52:14 +0200

    style: Adjusted container padding

--- pages/_app.js
+++ pages/_app.js
@@ -5,13 +5,13 @@ function MyApp({ Component, pageProps }) {
   return (
     <div className="h-full flex flex-col">
       <div className="sticky top-0 shadow-md bg-gray-800 text-white">
-        <div className="container mx-auto px-20">
+        <div className="container mx-auto md:px-52 px-10">
           <Header />
         </div>
       </div>
 
       <main className="bg-gray-100 pt-10 flex-auto">
-        <div className="container mx-auto px-20">
+        <div className="container mx-auto md:px-52 px-10 overflow-hidden">
           <Component {...pageProps} />
         </div>
       </main>
