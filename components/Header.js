ef9da87d0992442402dd8beb99899baa1f1f1476
Douglas Greyling <greyling.douglas@gmail.com>
2021-08-29 14:01:05 +0200

    fix: Fixed link href/as bug

--- components/Header.js
+++ components/Header.js
@@ -6,7 +6,7 @@ export default function Header() {
   return (
     <header>
       <div className="flex py-1.5 items-center">
-        <Link href={backendURL} as={backendURL + "/"}>
+        <Link href={backendURL + "/"} as={backendURL + "/"}>
           <a className="mr-auto hover:bg-gray-700 hover:no-underline p-3 hover:text-white py-1.5 rounded transition duration-200">
             <h1 className="text-2xl mb-0">Douglas Greyling</h1>
           </a>
